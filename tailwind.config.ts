03dfec28cfbb572d114e5fcf6c512057709fe88c
Sanjog Gururaj <sanjog.gururaj2@gmail.com>
2024-04-02 16:22:06 -0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        white: '0 0 10px rgba(255, 255, 255, 0.5)',
+      },
     },
   },
   plugins: [],
