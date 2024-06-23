f6f37f53b02a98c6a3680b91dae73e8ee81e4e27
Sanjog Gururaj <sanjog.gururaj2@gmail.com>
2024-06-23 16:08:13 -0500

    updated experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,14 +1,15 @@
 interface Props {
   title: string;
-  text: string;
-  icon: img;
+  subTitle: string;
+  icon: string;
 }
 
 const ExperienceCard = ({ title, subTitle, icon }: Props) => {
   return (
     <div className="flex items-center gap-4">
       <span className="w-12 h-12 rounded-full bg-black border-[1px] border-emerald-700 flex items-center justify-center">
-        <span className="text-2xl">{icon}</span>
+        {/* <span className="text-2xl">{icon}</span> */}
+        <img src={icon} alt="icon" className="w-full h-full" />
       </span>
       <div>
         <h3 className="text-2xl font-bold">{title}</h3>
